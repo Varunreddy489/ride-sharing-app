8d115e169314e5ff484dcf57b7ec10756ebada9d
Varunreddy489 <varunsannapureddy@gmail.com>
2026-06-03 17:32:04 +0530

    change project structure

---
+++ src/shared/api/register.py
@@ -0,0 +1,15 @@
+from types import ModuleType
+
+from fastapi import FastAPI
+
+
+def _register(uri_prefix: str, modules: list[ModuleType], app: FastAPI):
+    for module in modules:
+        app.include_router(module.router, prefix=uri_prefix)
+    return app
+
+
+def register_v1_routes(app: FastAPI) -> FastAPI:
+
+    v1_modules: list[ModuleType] = []
+    return _register("/v1", v1_modules, app)
